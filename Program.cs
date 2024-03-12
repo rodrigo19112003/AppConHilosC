ae42d2b13e956736c12539a5fff78e140490f0a3
rodrigo19112003 <zS21013854@estudiantes.uv.mx>
2024-03-11 22:17:33 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace AppConHilos;
+
+class Program
+{
+    public static void DoTrabajoPesado()
+    {
+        Console.WriteLine("DoTrabajoPesado: Estoy levantando un camión!");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: Cansado! Necesito una siesta de 3 seg.");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: 1....");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: 2....");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: 3....");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: Ya desperté.");
+    }
+
+    public static void DoAlgo()
+    {
+        Console.WriteLine("DoAlgo: Oye! Haciendo algo aquí");
+        for(int i = 0; i < 20; i++)
+            Console.Write($"{i} ");
+        Console.WriteLine();
+        Console.WriteLine("DoAlgo: Ya terminé.");
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("El Main thread comienza aquí.");
+
+        Thread backgroundThread = new Thread(new ThreadStart(Program.DoTrabajoPesado));
+        backgroundThread.Start();
+        Program.DoAlgo();
+
+        Console.WriteLine("El Main thread termina aquí");
+    }
+}
+
